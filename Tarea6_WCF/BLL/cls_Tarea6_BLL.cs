e15d2f4db3f7341916022a37bbd031cbecb26553
Manuel <Manuel@MANUEL-PC>
2019-03-14 23:21:42 -0600

    bll

--- Tarea6_WCF/BLL/cls_Tarea6_BLL.cs
+++ Tarea6_WCF/BLL/cls_Tarea6_BLL.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
 
 namespace BLL
 {
     public class cls_Tarea6_BLL
     {
+        cls_Tarea6_DAL Obj_DAL = new cls_Tarea6_DAL();
 
+        public void Encriptar()
+        {
+            Obj_DAL.bEncript = System.Text.Encoding.Unicode.GetBytes(Obj_DAL.sCadena);
+
+            Obj_DAL.sResultado = Convert.ToBase64String(Obj_DAL.bEncript);
+        }
+
+        public void Desencriptar()
+        {
+            Obj_DAL.bEncript = Convert.FromBase64String(Obj_DAL.sCadena);
+
+            Obj_DAL.sResultado = System.Text.Encoding.Unicode.GetString(Obj_DAL.bEncript);
+
+        }
     }
 }
--- Tarea6_WCF/DAL/cls_Tarea6_DAL.cs
+++ Tarea6_WCF/DAL/cls_Tarea6_DAL.cs
@@ -8,6 +8,53 @@ namespace DAL
 {
     public class cls_Tarea6_DAL
     {
+        #region Encriptado y desemcriptado
+        #region Variables Privadas
+        private string _sCadena, _sResultado;
+        byte[] _bEncript;
+        #endregion
 
+        #region Variables Públicas
+        public string sCadena
+        {
+            get
+            {
+                return _sCadena;
+            }
+
+            set
+            {
+                _sCadena = value;
+            }
+        }
+
+        public string sResultado
+        {
+            get
+            {
+                return _sResultado;
+            }
+
+            set
+            {
+                _sResultado = value;
+            }
+        }
+
+        public byte[] bEncript
+        {
+            get
+            {
+                return _bEncript;
+            }
+
+            set
+            {
+                _bEncript = value;
+            }
+        }
+        #endregion
+
+        #endregion
     }
 }
